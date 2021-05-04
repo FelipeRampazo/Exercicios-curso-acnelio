7eb3573f9278a45a57ee3da64a87f709b3617fec
felipe_a_r@hotmail.com <felipe_a_r@hotmail.com>
2021-05-04 12:14:50 -0300

    adicionado o exercicio de tratamento de excecoes (ExeExec)

---
+++ ExeExec/ExeExec/Entities/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExeExec.Entities.Exceptions;
+
+namespace ExeExec.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double  Balance { get; set; }
+
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double ammount)
+        {
+            Balance += ammount;
+        }
+
+        public void Withdraw(double ammount)
+        {
+            
+            if (ammount > Balance)
+            {
+                throw new DomainException("valor do saque é maior que o valor do saldo");
+            }
+            if (ammount > WithdrawLimit)
+            {
+                throw new DomainException("valor não pode ser maior que o limite de saque ");
+            }
+
+            Balance -= ammount;
+        }
+    }
+}
---
+++ ExeExec/ExeExec/Program.cs
@@ -0,0 +1,48 @@
+﻿using ExeExec.Entities;
+using System;
+using System.Globalization;
+using ExeExec.Entities.Exceptions;
+
+namespace ExeExcep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data: ");
+
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+
+                Console.Write("Inicial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter ammout for withdraw: ");
+                double ammount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                account.Withdraw(ammount);
+
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2",CultureInfo.InvariantCulture));
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("excessao : "+ e.Message );
+            }
+
+
+        }
+    }
+}
